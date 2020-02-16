f2bee623f4f96da797867b2ab12c0f19ad2b494b
lukaszdyszy <mruhol123@gmail.com>
2020-02-16 21:43:13 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+* {
+  box-sizing: border-box;
+}
+
+body{
+  margin: 0;
+  padding: 0;
+  font-family: 'Open Sans', sans-serif;
+}
+
+</style>
